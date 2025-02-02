88aafc70a0168d59d636a9aee0ed285e03e99d40
Taptapo <y_zhumatay@kbtu.kz>
2025-02-02 23:10:40 +0600

    Add files via upload

---
+++ lab2/if_else/exr.py
@@ -0,0 +1,63 @@
+#1-exr
+a = 50
+b = 10
+if a > b:
+    print("Hello World")
+
+#2-exr
+a = 50
+b = 10
+if a != b:
+    print("Hello World")
+
+#3-exr
+a = 50
+b = 10
+if a == b:
+    print("Yes")
+else:
+    print("No")
+
+#4-exr
+a = 50
+b = 10
+if a ==  b:
+    print("1")
+elif a >b:
+    print("2")
+else:
+    print("3")
+
+#5-exr
+a=1
+b=1
+c=2
+d=2
+
+if a == b and c == d:
+  print("Hello")
+
+#6-exr
+a=1
+b=2
+c=1
+d=1
+
+if a == b or c == d:
+  print("Hello")
+
+#7-exr
+if 5 > 2:
+    print("Yes")
+
+#8-exr
+a = 2
+b = 5
+print("YES") if a==b else print("NO")
+
+#9-exr
+a = 2
+b = 50
+c = 2
+if a==c or b==c:
+    print("YES")
